047e6d517085afb9bee94a4d4130b22dc47a97eb
giaolinh99 <giaolinhdo.com>
2021-07-09 17:03:27 +0700

    bo dispose

--- lib/views/alarm_page.dart
+++ lib/views/alarm_page.dart
@@ -69,11 +69,11 @@ class _AlarmPageState extends State<AlarmPage> {
     if (mounted) setState(() {});
   }
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   // TODO: implement dispose
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
