efd41d437fa7ccc73e97fb6fa3508997cb1f3d16
giaolinh99 <giaolinhdo.com>
2021-07-09 11:34:38 +0700

    update 0907-ver3

--- lib/views/alarm_page.dart
+++ lib/views/alarm_page.dart
@@ -70,6 +70,12 @@ class _AlarmPageState extends State<AlarmPage> {
   }
 
   @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
 
    // reminderLogtime();
