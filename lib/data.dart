51325ae9e8bad464457165685b701d92b14caea8
giaolinh99 <giaolinhdo.com>
2021-07-09 11:57:23 +0700

    sua homepage1 -> linh

--- lib/data.dart
+++ lib/data.dart
@@ -5,13 +5,13 @@ import 'models/menu_info.dart';
 
 List<MenuInfo> menuItems = [
   MenuInfo(MenuType.clock,
-      title: 'Clock', imageSource: 'assets/clock_icon.png'),
+      title: 'Clock', imageSource: 'assets/images/clock_icon.png'),
   MenuInfo(MenuType.alarm,
-      title: 'Alarm', imageSource: 'assets/alarm_icon.png'),
+      title: 'Alarm', imageSource: 'assets/images/alarm_icon.png'),
   MenuInfo(MenuType.timer,
-      title: 'Timer', imageSource: 'assets/timer_icon.png'),
+      title: 'Timer', imageSource: 'assets/images/timer_icon.png'),
   MenuInfo(MenuType.stopwatch,
-      title: 'Stopwatch', imageSource: 'assets/stopwatch_icon.png'),
+      title: 'Stopwatch', imageSource: 'assets/images/stopwatch_icon.png'),
 ];
 
 List<AlarmInfo> alarms = [
